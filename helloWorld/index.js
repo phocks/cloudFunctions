2b3daf4d7a8178579239cd5e615aa3525b7b458e
Joshua Byrd <phocks@gmail.com>
2017-11-02 16:20:19 +1000

    It begins.

---
+++ helloWorld/index.js
@@ -0,0 +1,50 @@
+function handleGET(req, res) {
+  // Do something with the GET request
+  var input = req.query.input;
+
+  var randomNumber = Math.floor((Math.random() * input) + 1);
+  res.status(200).json({
+    messages: [{ text: "Your random number is: " + randomNumber }]
+  });
+}
+
+function handlePUT(req, res) {
+  // Do something with the PUT request
+  res.status(403).send("Forbidden!");
+}
+
+function handlePOST(req, res) {
+  // Do something with the POST request
+  var input = req.body.input;
+
+  var randomNumber = Math.floor((Math.random() * input) + 1);
+  res.status(200).json({
+    messages: [{ text: "Your random number is: " + String(randomNumber) }]
+  });
+}
+
+/**
+ * Responds to a GET request with "Hello World!". Forbids a PUT request.
+ *
+ * @example
+ * gcloud alpha functions call helloHttp
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.helloWorld = function helloHttp(req, res) {
+  switch (req.method) {
+    case "GET":
+      handleGET(req, res);
+      break;
+    case "PUT":
+      handlePUT(req, res);
+      break;
+    case "POST":
+      handlePOST(req, res);
+      break;
+    default:
+      res.status(500).send({ error: "Something blew up!" });
+      break;
+  }
+};
